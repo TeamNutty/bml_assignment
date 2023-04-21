777ccf8ccb1998fa9bb36e58e82b149e1f2eccc4
team <nuttadol.h@geotalent.co.th>
2023-04-21 09:19:12 +0700

    setup assignment

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
+import Container from "./components/Container";
+import Header from "./components/Header";
 
 function App() {
-  return <div className="App">ddgdgd</div>;
+  return (
+    <div className="bangkok-population-growth">
+      <Container>
+        <Header />
+      </Container>
+    </div>
+  );
 }
 
 export default App;
